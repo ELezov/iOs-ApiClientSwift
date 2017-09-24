c57c28e159e64b1ccd61cef88b67e54a5ca3cd0f
Eugene Lezov <Elezov@stud.kantiana.ru>
2017-09-24 15:07:15 +0200

    Initial Commit

---
+++ TestApiClientSwift_iOs/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestApiClientSwift_iOs
+//
+//  Created by KODE_H6 on 24.09.17.
+//  Copyright © 2017 KODE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
