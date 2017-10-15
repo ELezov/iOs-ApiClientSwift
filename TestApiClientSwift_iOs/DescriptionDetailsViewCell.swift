9e259d64a3bf180f32b15c3a4aae472a4cec4127
Eugene <ELezov@stud.kantiana.ru>
2017-10-15 12:24:36 +0200

    Начал работать над перестройкой верстки на экране детальной информации

---
+++ TestApiClientSwift_iOs/DescriptionDetailsViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  DescriptionDetailsViewCell.swift
+//  TestApiClientSwift_iOs
+//
+//  Created by Nikolay on 14.10.17.
+//  Copyright © 2017 KODE. All rights reserved.
+//
+
+import UIKit
+
+class DescriptionDetailsViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ TestApiClientSwift_iOs/HeaderPlaceDetailsCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  TestApiClientSwift_iOs
+//
+//  Created by Nikolay on 14.10.17.
+//  Copyright © 2017 KODE. All rights reserved.
+//
+
+import UIKit
+
+class HeaderPlaceDetailsViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
